8eb29c1a48bde8915f09ee1657f0ada8b93fc2be
Spoutnikrs <rayesyounes88@gmail.com>
2024-02-15 23:41:53 +0100

    Diamond Pattern

---
+++ Diamond Pattern/main.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number of rows: "));
+
+
+def print_diamond(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "*" * (2 * i - 1))
+
+    for i in range(n - 1, 0, -1):
+        print(" " * (n - i) + "*" * (2 * i - 1))
+
+
+print_diamond(n)
